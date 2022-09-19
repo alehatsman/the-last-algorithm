813958913048b7c73a7b03a71e230eff330967c9
Aleh Atsman <aleh.atsman@gmail.com>
2022-09-19 09:43:41 +0200

    sync

---
+++ src/day1/TwoCrystalBalls.ts
@@ -0,0 +1,19 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmnt = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jmpAmnt;
+    for (; i < breaks.length; i += jmpAmnt) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jmpAmnt;
+    for (let j = 0; j < jmpAmnt && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+}
