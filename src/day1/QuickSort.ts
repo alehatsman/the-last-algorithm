ee6892449e9a53938e0f1796cf6e95c04c2de8e3
Aleh Atsman <aleh.atsman@gmail.com>
2022-09-23 09:34:50 +0200

    add quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,1 +1,33 @@
-export default function quick_sort(arr: number[]): void {}
+function qs(arr: number[], lo: number, hi: number): void {
+  if (lo >= hi) {
+    return;
+  }
+
+  const pivotIdx = pt(arr, lo, hi);
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx + 1, hi);
+}
+
+function pt(arr: number[], lo: number, hi: number): number {
+  const pivot = arr[hi];
+  let idx = lo - 1;
+
+  for (let i = lo; i < hi; i += 1) {
+    if (arr[i] <= pivot) {
+      idx += 1;
+      const temp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = temp;
+    }
+  }
+
+  idx += 1;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+
+  return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+  qs(arr, 0, arr.length - 1);
+}
