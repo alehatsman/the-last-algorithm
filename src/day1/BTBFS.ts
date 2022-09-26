c87a68d15df9c0b3c34c5155d106b76ff624bd5d
Aleh Atsman <aleh.atsman@gmail.com>
2022-09-26 18:59:27 +0200

    breadth-first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,4 +1,21 @@
-export default function bfs(
-  head: BinaryNode<number>,
-  needle: number,
-): boolean {}
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const queue = [head];
+
+  while (queue.length > 0) {
+    const currentNode = queue.shift()!;
+
+    if (currentNode.value === needle) {
+      return true;
+    }
+
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+  }
+
+  return false;
+}
