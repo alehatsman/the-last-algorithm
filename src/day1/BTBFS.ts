1d00c46fe6720877b107c084e1d31c969e24713b
Aleh Atsman <aleh.atsman@gmail.com>
2022-09-26 19:09:44 +0200

    breadth-first search with recustion

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,4 +1,4 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+function bfs_with_loop(head: BinaryNode<number>, needle: number): boolean {
   const queue = [head];
 
   while (queue.length > 0) {
@@ -18,4 +18,29 @@ export default function bfs(head: BinaryNode<number>, needle: number): boolean {
   }
 
   return false;
+}
+
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  return walk([head], needle);
+}
+
+function walk(queue: BinaryNode<number>[], needle: number): boolean {
+  const node = queue.shift();
+  if (!node) {
+    return false;
+  }
+
+  if (node.value === needle) {
+    return true;
+  }
+
+  if (node.left) {
+    queue.push(node.left);
+  }
+
+  if (node.right) {
+    queue.push(node.right);
+  }
+
+  return walk(queue, needle);
 }
