d2e96a5d60c7363c9f6c64209723a0e71075ecc2
Aleh Atsman <aleh.atsman@gmail.com>
2022-10-04 18:15:16 +0200

    while loop is nicer

--- src/__tests__/MinHeap.ts
+++ src/__tests__/MinHeap.ts
@@ -1,28 +1,46 @@
 import MinHeap from "@code/MinHeap";
 
-test("min heap", function() {
-  const heap = new MinHeap();
+describe("MinHeap", () => {
+  test("insert and bubble up", () => {
+    const heap = new MinHeap();
 
-  expect(heap.length).toEqual(0);
+    heap.insert(100);
+    expect(heap.heap).toEqual([100]);
 
-  heap.insert(5);
-  heap.insert(3);
-  heap.insert(69);
-  heap.insert(420);
-  heap.insert(4);
-  heap.insert(1);
-  heap.insert(8);
-  heap.insert(7);
+    heap.insert(87);
+    expect(heap.heap).toEqual([87, 100]);
 
-  expect(heap.length).toEqual(8);
-  expect(heap.delete()).toEqual(1);
-  expect(heap.delete()).toEqual(3);
-  expect(heap.delete()).toEqual(4);
-  expect(heap.delete()).toEqual(5);
-  expect(heap.length).toEqual(4);
-  expect(heap.delete()).toEqual(7);
-  expect(heap.delete()).toEqual(8);
-  expect(heap.delete()).toEqual(69);
-  expect(heap.delete()).toEqual(420);
-  expect(heap.length).toEqual(0);
+    heap.insert(88);
+    expect(heap.heap).toEqual([87, 100, 88]);
+
+    heap.insert(3);
+    expect(heap.heap).toEqual([3, 87, 88, 100]);
+  });
+
+  test("min heap", function () {
+    const heap = new MinHeap();
+
+    expect(heap.length).toEqual(0);
+
+    heap.insert(5);
+    heap.insert(3);
+    heap.insert(69);
+    heap.insert(420);
+    heap.insert(4);
+    heap.insert(1);
+    heap.insert(8);
+    heap.insert(7);
+
+    expect(heap.length).toEqual(8);
+    expect(heap.delete()).toEqual(1);
+    expect(heap.delete()).toEqual(3);
+    expect(heap.delete()).toEqual(4);
+    expect(heap.delete()).toEqual(5);
+    expect(heap.length).toEqual(4);
+    expect(heap.delete()).toEqual(7);
+    expect(heap.delete()).toEqual(8);
+    expect(heap.delete()).toEqual(69);
+    expect(heap.delete()).toEqual(420);
+    expect(heap.length).toEqual(0);
+  });
 });
--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,6 +1,6 @@
 export default class MinHeap {
   public length: number;
-  private heap: number[];
+  public heap: number[];
 
   constructor() {
     this.length = 0;
@@ -9,8 +9,8 @@ export default class MinHeap {
 
   insert(value: number): void {
     this.heap.push(value);
-    this.bubbleUp(this.length);
     this.length += 1;
+    this.bubbleUp();
   }
 
   delete(): number {
@@ -18,53 +18,48 @@ export default class MinHeap {
       return -1;
     }
 
-    if (this.length === 1) {
-      const temp = this.heap[0];
+    const temp = this.heap[0];
+    this.length -= 1;
+    if (this.length === 0) {
       this.heap = [];
-      this.length = 0;
       return temp;
     }
 
-    const temp = this.heap[0];
-    this.length -= 1;
     this.heap[0] = this.heap[this.length];
-    this.bubbleDown(0);
+    this.bubbleDown();
     return temp;
   }
 
-  private bubbleUp(i: number): void {
-    if (i === 0) {
-      return;
-    }
-
-    const pi = this.parent(i);
-    const pv = this.heap[pi];
-    const iv = this.heap[i];
-
-    if (pv > iv) {
-      this.swap(pi, i);
-      this.bubbleUp(pi);
+  private bubbleUp(): void {
+    let index = this.length - 1;
+    while (index !== 0) {
+      let parentIndex = this.parent(index);
+      if (this.heap[index] < this.heap[parentIndex]) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
+      }
     }
   }
 
-  private bubbleDown(i: number): void {
-    const li = this.leftChild(i);
-    const ri = this.rightChild(i);
+  private bubbleDown(): void {
+    let index = 0;
+    while (index < this.length - 1) {
+      let leftChildIndex = this.leftChild(index);
+      let rightChildIndex = this.rightChild(index);
 
-    if (i >= this.length || li >= this.length) {
-      return;
-    }
+      let minimalChildIndex = leftChildIndex;
+      if (this.heap[rightChildIndex] < this.heap[leftChildIndex]) {
+        minimalChildIndex = rightChildIndex;
+      }
 
-    const lv = this.heap[li];
-    const rv = this.heap[ri];
-    const v = this.heap[i];
-
-    if (lv > rv && v > rv) {
-      this.swap(i, ri);
-      this.bubbleDown(ri);
-    } else if (rv > lv && v > lv) {
-      this.swap(i, li);
-      this.bubbleDown(li);
+      if (this.heap[index] > this.heap[minimalChildIndex]) {
+        this.swap(index, minimalChildIndex);
+      } else {
+        break;
+      }
+      index = minimalChildIndex;
     }
   }
 
