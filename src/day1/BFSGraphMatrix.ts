a2d7c3cba26443e2738c44517e98371bf9f91da3
Aleh Atsman <aleh.atsman@gmail.com>
2022-10-11 13:19:25 +0200

    graphs are not fun

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -2,4 +2,47 @@ export default function bfs(
   graph: WeightedAdjacencyMatrix,
   source: number,
   needle: number,
-): number[] | null { }
+): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  seen[source] = true;
+  const prev = new Array(graph.length).fill(-1);
+  const queue = [source];
+
+  while (queue.length) {
+    const current = queue.shift()!;
+
+    if (current === needle) {
+      break;
+    }
+
+    const edges = graph[current];
+    for (let i = 0; i < edges.length; i += 1) {
+      const weight = edges[i];
+      if (weight === 0) {
+        continue;
+      }
+
+      if (seen[i]) {
+        continue;
+      }
+
+      seen[i] = true;
+      prev[i] = current;
+      queue.push(i);
+    }
+  }
+
+  if (prev[needle] === -1) {
+    return null;
+  }
+
+  const path = [];
+  let current = needle;
+  while (current !== -1) {
+    path.push(current);
+    current = prev[current];
+  }
+
+  path.reverse();
+  return path;
+}
--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,5 +1,46 @@
+function walk(
+  graph: WeightedAdjacencyList,
+  curr: number,
+  needle: number,
+  seen: boolean[],
+  path: number[],
+): boolean {
+  if (seen[curr]) {
+    return false;
+  }
+  seen[curr] = true;
+
+  path.push(curr);
+
+  if (curr === needle) {
+    return true;
+  }
+
+  const vertex = graph[curr];
+  for (let i = 0; i < vertex.length; i += 1) {
+    const edge = vertex[i];
+    const found = walk(graph, edge.to, needle, seen, path);
+    if (found) {
+      return true;
+    }
+  }
+
+  path.pop();
+
+  return false;
+}
+
 export default function dfs(
   graph: WeightedAdjacencyList,
   source: number,
   needle: number,
-): number[] | null {}
+): number[] | null {
+  const seen = new Array(graph.length).fill(false);
+  const path: number[] = [];
+
+  if (walk(graph, source, needle, seen, path)) {
+    return path;
+  }
+
+  return null;
+}
